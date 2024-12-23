a2ba3b2cad37c8e2a23ebfd76683846459e85469
bensoro2 <wongsathon.y@cvc.ac.th>
2024-12-23 22:31:07 +0700

    create web

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const authRoutes = require("./src/routes/auth");
+const transactionRoutes = require("./src/routes/transaction");
+const projectRoutes = require("./src/routes/project");
+const subsidyRoutes = require("./src/routes/subsidy");
+const fiscalYearRoutes = require("./src/routes/fiscalYear");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use("/auth", authRoutes);
+app.use("/transactions", transactionRoutes);
+app.use("/projects", projectRoutes);
+app.use("/subsidies", subsidyRoutes);
+app.use("/fiscal-years", fiscalYearRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
